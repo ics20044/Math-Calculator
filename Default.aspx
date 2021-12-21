926406d9781a7abde0779218fd24d037089d3038
Panagiotis Machairas <ics20044@uom.edu.gr>
2021-12-21 18:27:33 +0200

    First commit in Math Calculator.

---
+++ Default.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <h2>Calculator</h2>
+        <p>
+            number A<asp:TextBox ID="TextBoxNumberA" runat="server"></asp:TextBox>
+        </p>
+    
+    </div>
+        <asp:Button ID="sum" runat="server" Text="+" OnClick="sum_Click" Height="28px" Width="56px" />
+        <asp:Button ID="minus" runat="server" Text="-" OnClick="minus_Click" Width="41px" />
+        <asp:Button ID="multiply" runat="server" Text="x" OnClick="multiply_Click" Width="44px" />
+        <asp:Button ID="divide" runat="server" Text="/" OnClick="divide_Click" Width="38px" />
+        <br />
+        <br />
+        number B<asp:TextBox ID="TextBoxNumberB" runat="server"></asp:TextBox>
+        <br />
+        Result
+        <asp:Label ID="resultText" runat="server"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    com.dneonline.www.Calculator calculator = new com.dneonline.www.Calculator();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+      
+    }
+
+    protected void sum_Click(object sender, EventArgs e)
+    {
+        int a, b, result;
+        a = Convert.ToInt32(TextBoxNumberA.Text);
+        b = Convert.ToInt32(TextBoxNumberB.Text);
+        result = calculator.Add(a, b);
+        resultText.Text = Convert.ToString(result);
+    }
+
+    protected void minus_Click(object sender, EventArgs e)
+    {
+        int a, b, result;
+        a = Convert.ToInt32(TextBoxNumberA.Text);
+        b = Convert.ToInt32(TextBoxNumberB.Text);
+        result = calculator.Subtract(a, b);
+        resultText.Text = Convert.ToString(result);
+    }
+
+    protected void multiply_Click(object sender, EventArgs e)
+    {
+        int a, b, result;
+        a = Convert.ToInt32(TextBoxNumberA.Text);
+        b = Convert.ToInt32(TextBoxNumberB.Text);
+        result = calculator.Multiply(a, b);
+        resultText.Text = Convert.ToString(result);
+    }
+
+    protected void divide_Click(object sender, EventArgs e)
+    {
+        int a, b, result;
+        a = Convert.ToInt32(TextBoxNumberA.Text);
+        b = Convert.ToInt32(TextBoxNumberB.Text);
+        result = calculator.Divide(a, b); 
+        resultText.Text = Convert.ToString(result);
+    }
+}
